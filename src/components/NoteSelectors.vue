e5c9432d423fd63679bb5f27363ab7da5aed177b
Mabelys Arias <MabelysA@gmail.com>
2021-08-03 17:37:25 -0400

    adds v-for to generate each note selector, passes selectedNoteId into NoteSelector using v-bind:class

--- src/components/NoteSelectors.vue
+++ src/components/NoteSelectors.vue
@@ -1,20 +1,12 @@
 <template>
   <div class="note-selectors">
-    <!-- <div class="note-selector active">
-      <p class="note-selector-title">First note...</p>
-      <p class="note-selector-timestamp">Timestamp here...</p>
-    </div>
-    <div class="note-selector">
-      <p class="note-selector-title">Second note...</p>
-      <p class="note-selector-timestamp">Timestamp here...</p>
-    </div>
-    <div class="note-selector">
-      <p class="note-selector-title">Third note...</p>
-      <p class="note-selector-timestamp">Timestamp here...</p>
-    </div> -->
-    <NoteSelector />
-    <NoteSelector />
-    <NoteSelector />
+    <NoteSelector
+      v-for="note in notes"
+      v-bind:note="note"
+      v-bind:selectedNoteId="selectedNoteId"
+      v-on:selectNote="selectNote"
+      :key="note.id"
+    />
   </div>
 </template>
 
@@ -24,6 +16,22 @@ import NoteSelector from "./NoteSelector.vue";
 export default {
   components: {
     NoteSelector,
+  },
+  data: function () {
+    return {
+      notes: [
+        { id: 1, body: "This is a first test", timestamp: Date.now() },
+        { id: 2, body: "This is a second test", timestamp: Date.now() },
+        { id: 3, body: "This is a third test", timestamp: Date.now() },
+        { id: 4, body: "This is a fourth test", timestamp: Date.now() },
+      ],
+      selectedNoteId: 1,
+    };
+  },
+  methods: {
+    selectNote: function (note) {
+      this.selectedNoteId = note.id;
+    },
   },
 };
 </script>
